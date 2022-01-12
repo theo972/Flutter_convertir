d98b2b3738421b26f7aaf837973657a2c397a44e
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-01-12 19:26:01 +0100

    premier commit

--- flutter_convertir/lib/Romain_page.dart
+++ flutter_convertir/lib/Romain_page.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class RomePage extends StatelessWidget {
-  static const tag = "Rome_page";
+class RomainPage extends StatefulWidget {
+  static const tag = "";
+
+  @override
+  _RomainPageState createState() => _RomainPageState();
+}
+
+class _RomainPageState extends State<RomainPage> {
+  String formFieldValue = "0";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(),);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Convertir en chiffre Romain"),
+          centerTitle: true,
+        ),
+        body: Container(
+          child: Column(children: [
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: false),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}'))
+              ],
+              decoration: InputDecoration(hintText: 'Entrez un nombre'),
+              onChanged: (String? newTextFieldValue) {
+                setState(() {
+                  formFieldValue = newTextFieldValue!;
+                });
+              },
+            ),
+          ]),
+        ));
   }
 }
