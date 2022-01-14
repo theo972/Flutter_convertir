51c7420b09769e00d3d6e9a8cabcf5b021b95b6c
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2022-01-14 13:44:20 +0100

    bin oct resolved

--- flutter_convertir/lib/binary_pages.dart
+++ flutter_convertir/lib/binary_pages.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 
 class BinaryPage extends StatefulWidget {
   static const tag = "Binary_page";
@@ -12,68 +15,105 @@ _HomePageState createState() => _HomePageState();
 
 class _HomePageState extends State<BinaryPage> {
 
+  decimale(text) {
+    String decoctal = '';
+    String deci = text;
+    String decbinaire = '';
+    String dechexadecimal='';
+     int decbin = int.parse(text);
+    int decoct = int.parse(text);
+    int dechexa = int.parse(text);
+    if(text != null) {
 
-  toto(text, String) {
+      for (int rest = 0; dechexa > 0; dechexa = dechexa ~/ 16) {
+        String resteh = '';
+        log(decbin);
+        rest = dechexa % 16;
+        if(rest == 10) {
+          resteh = 'A';
+          dechexadecimal = resteh + dechexadecimal;
+        }
+        if(rest == 11){
 
-    if(text != 0 && String == 'Bin') {
-      binair = (double.parse('$text')).toDouble();
-      octal = (double.parse('$text') * 1e6).toDouble();
-      hexa = (double.parse('$text') * 1e5).toDouble();
-      decimetre = (double.parse('$text') * 10000).toDouble();
-
+          resteh = 'B';
+          dechexadecimal = resteh + dechexadecimal ;
+        }if(rest == 12){
+          resteh = 'C';
+          dechexadecimal = resteh + dechexadecimal ;
+        }if(rest == 13){
+          resteh = 'D';
+          dechexadecimal = resteh + dechexadecimal ;
+        }if(rest == 14){
+          resteh = 'E';
+          dechexadecimal = resteh + dechexadecimal ;
+        }if(rest == 15){
+          resteh = 'F';
+          dechexadecimal = resteh + dechexadecimal ;
+        }
+        else{
+          resteh = rest.toString();
+          dechexadecimal = resteh + dechexadecimal ;
+        }
+        log(rest);
+      }
+      for (int rest = 0; decbin > 0; decbin = decbin ~/ 2) {
+        log(decbin);
+        rest = decbin % 2;
+        log(rest);
+        String resteb = rest.toString();
+        decbinaire = resteb + decbinaire ;
+      }
+      for (int rest = 0; decoct > 0; decoct = decoct ~/ 8) {
+        log(decoct);
+        rest = decoct % 8;
+        log(rest);
+        String reste = rest.toString();
+        decoctal = reste + decoctal ;
+      }
     }
-    if(text != 0 && String == 'octal') {
-      binair = (double.parse('$text') * 9.144e8).toDouble();
-      octal =  (double.parse('$text')).toDouble();
-      hexa = (double.parse('$text') * 91.44).toDouble();
-      decimetre = (double.parse('$text') * 9.144).toDouble();
+    myControllerbin.text = decbinaire;
+    myControllerbin.selection = TextSelection.fromPosition(TextPosition(offset: myControllerbin.text.length));
+    myControlleroctal.text = decoctal;
+    myControlleroctal.selection = TextSelection.fromPosition(TextPosition(offset: myControlleroctal.text.length));
+    myControllerhex.text = dechexadecimal;
+    myControllerhex.selection = TextSelection.fromPosition(TextPosition(offset: myControllerhex.text.length));
+    myControllerdeci.text = deci;
+    myControllerdeci.selection = TextSelection.fromPosition(TextPosition(offset: myControllerdeci.text.length));
 
-    }
-    if(text != 0 && String == 'Decimal') {
-      binair = (double.parse('$text') * 3.048e+8).toDouble();
-      octal = (double.parse('$text') * 305).toDouble();
-      hexa =  (double.parse('$text')).toDouble();
-      decimetre = (double.parse('$text') * 3.048).toDouble();
+  }
+  binaire(text){
 
-    }
-    if(text != 0 && String == 'Hex') {
-      binair = (double.parse('$text') * 2.54e+7).toDouble();
-      octal = (double.parse('$text') * 25.4).toDouble();
-      hexa =  (double.parse('$text')).toDouble();
-      decimetre = (double.parse('$text') / 3.937).toDouble();
-
-    }
-    setState(() {
-      myControllerbin.text = binair.toString();
-      myControllerbin.selection = TextSelection.fromPosition(TextPosition(offset: myControllerbin.text.length));
-      myControlleroctal.text = octal.toString();
-      myControlleroctal.selection = TextSelection.fromPosition(TextPosition(offset: myControlleroctal.text.length));
-      myControllerdeci.text = decimetre.toString();
-      myControllerdeci.selection = TextSelection.fromPosition(TextPosition(offset: myControllerdeci.text.length));
-      myControllerhex.text = hexa.toString();
-      myControllerhex.selection = TextSelection.fromPosition(TextPosition(offset: myControllerhex.text.length));
-
-    });
-
+    String bintodec = int.parse(text, radix: 2).toString();
+    decimale(bintodec);
 
   }
 
-  static const String _title = "Conversion de donnée";
-
-  double binair =0;
-  double octal =0;
-  double hexa = 0;
-  double decimetre =0;
-
-
-  final myControllerbin = TextEditingController();
-  final myControlleroctal = TextEditingController();
-  final myControllerdeci = TextEditingController();
-  final myControllerhex = TextEditingController();
+  Octal(text){
+    String octtodec = int.parse(text, radix: 8).toString();
+    decimale(octtodec);
+  }
+  Hexadecimal(text){
+    String hextodec = int.parse(text, radix: 16).toString();
+    decimale(hextodec);
+  }
 
 
-  @override
+    static const String _title = "Conversion de donnée";
+    int debut =0;
+    int binair =0;
+    int octal =0;
+    int hexa = 0;
+    int deci =0;
+
+
+    final myControllerbin = TextEditingController();
+    final myControlleroctal = TextEditingController();
+    final myControllerdeci = TextEditingController();
+    final myControllerhex = TextEditingController();
+
+    @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_title),
@@ -88,7 +128,7 @@ class _HomePageState extends State<BinaryPage> {
               child: TextField(
                 controller: myControllerbin,
                 onChanged: (text) {
-                  toto(text,'Bin');
+                  binaire(text);
                 },
                 style: TextStyle(
                   fontSize: 12.0,
@@ -101,7 +141,8 @@ class _HomePageState extends State<BinaryPage> {
                 ),
                 keyboardType: TextInputType.number,
                 inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
+                  FilteringTextInputFormatter.allow(RegExp('[0-1]',))
+                  
                 ], // Only numbers can be entered
               ),
             ),
@@ -111,7 +152,7 @@ class _HomePageState extends State<BinaryPage> {
               child: TextField(
                 controller: myControlleroctal,
                 onChanged: (text) {
-                  toto(text,'octal');
+                  Octal(text);
                 },
                 style: TextStyle(
                   fontSize: 12.0,
@@ -124,8 +165,8 @@ class _HomePageState extends State<BinaryPage> {
                 ),
                 keyboardType: TextInputType.number,
                 inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ], // Only numbers can be entered
+                  FilteringTextInputFormatter.allow(RegExp('[0-7]',))
+                ],
               ),
             ),
             Container(
@@ -134,7 +175,7 @@ class _HomePageState extends State<BinaryPage> {
               child: TextField(
                 controller: myControllerdeci,
                 onChanged: (text) {
-                  toto(text,'Decimal');
+                  decimale(text);
                 },
                 style: TextStyle(
                   fontSize: 12.0,
@@ -157,7 +198,7 @@ class _HomePageState extends State<BinaryPage> {
               child: TextField(
                 controller: myControllerhex,
                 onChanged: (text) {
-                  toto(text,'Hex');
+                  Hexadecimal(text);
                 },
                 style: TextStyle(
                   fontSize: 12.0,
@@ -168,10 +209,6 @@ class _HomePageState extends State<BinaryPage> {
                   border: OutlineInputBorder(),
                   labelText: 'Hexadecimal',
                 ),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ], // Only numbers can be entered
               ),
             ),
           ],
